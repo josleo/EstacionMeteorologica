c3cbb614e56766579dc68f318024f6346fca4421
joselio <milquiann@gmail.com>
2022-07-19 09:19:09 +1030

    almacena solo las lecuras solos los 0 que captura el sensorr y los almacena en una txt

--- JNK-50001c.py
+++ JNK-50001c.py
@@ -1,7 +1,7 @@
 from time import sleep
 import RPi.GPIO as GPIO
 import csv
-
+import time
 # Set up BCM GPIO numbering.
 GPIO.setmode(GPIO.BCM)
 # Set up input pins.
@@ -9,7 +9,7 @@ SENSOR_1_INPUT = 18
 GPIO.setup(SENSOR_1_INPUT, GPIO.IN)
 
 lista = []
-lista2 = ["2"]
+lista2 = ["1"]
 #datos =  pd.DataFrame()
 # Initiate the loop.
 while True:
@@ -18,17 +18,23 @@ while True:
     # Print values.
     if(SENSOR_1_VALUE == 0):
         lista.append(str(SENSOR_1_VALUE))
+        valorSTR = str(SENSOR_1_VALUE)
         print (SENSOR_1_VALUE)
 #    datos['Nombre'] = lista
 #    datos.append(SENSOR_1_VALUE)
         with open('Phj.txt', 'a', newline='') as csvfile:
-            csvfile.write(','.join(lista)+'\n')
+            csvfile.write(','.join(valorSTR)+'\n')
+            #csvfile.close()
+            print (SENSOR_1_VALUE)
+            time.sleep(1)
 #            break
 
     else:
-        with open('Phj.txt', 'a', newline='') as csvfile:
-            csvfile.write(','.join(lista2)+'\n')
+#        with open('Phj.txt', 'a', newline='') as csvfile:
+#            csvfile.write(','.join(lista2)+'\n')
 #            break
+            print (lista2)
+            time.sleep(1)
 
 #datos['Nombre'] = lista
 
