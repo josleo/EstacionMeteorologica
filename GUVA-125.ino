74b85e3bbc7c13a8eaf384ace4cb773ebe9f7a7a
joselio <milquiann@gmail.com>
2022-08-08 20:51:14 +0100

    se cambio de sensor UV  ahora se esta usando el modelo  gyml8511

--- GUVA-125.ino
+++ GUVA-125.ino
@@ -1,46 +1,59 @@
-int uv=A0, lectura;
+//Hardware pin definitions
+int UVOUT = A0; //Output from the sensor
+int REF_3V3 = A1; //3.3V power on the Arduino board
 
-String riesgo="";
-void setup() {
+void setup()
+{
   Serial.begin(9600);
 
+  pinMode(UVOUT, INPUT);
+  pinMode(REF_3V3, INPUT);
+
+  //Serial.println("MP8511 example");
 }
 
-void loop() {
-  lectura=analogRead(uv);
-  //float voltaje=lectura * (5.0/1023.0);
-  float voltaje=lectura * (3.3/669);
-  int longOnda=map(lectura,0,669,0,1023);
-  int indice=map(longOnda,0,1023,0,10);
-  //Serial.println(lectura);
-  //Serial.println("Voltaje:"+String(voltaje)+"V");
-  //Serial.println("Rango:"+String(longOnda));
-  if(indice==0){
-    riesgo="0";
-    
-  }
-  else if(indice==1 or indice ==2){
-    riesgo="1";
-  }
-  else if(indice>=3 and indice <=5){
-    riesgo="2";
-  }
-  else if(indice>=6 and indice <=7){
-    riesgo="3";
-  }
-  else if(indice>=8 and indice <=10){
-    riesgo="4";
+void loop()
+{
+  int uvLevel = averageAnalogRead(UVOUT);
+  int refLevel = averageAnalogRead(REF_3V3);
   
-  }
-  else {
-    riesgo="5";
-  }
-  //Serial.println("Indice UV:"+String(indice)+" - Rango:"+String(longOnda));
-  Serial.print (riesgo);
-  Serial.print("  ");
-  Serial.print(lectura);
-  Serial.print("Voltaje:"+String(voltaje)+"V");
-  Serial.println("Rango:"+String(longOnda));
+  //Use the 3.3V power pin as a reference to get a very accurate output value from sensor
+  float outputVoltage = 3.3 / refLevel * uvLevel;
+  
+  float uvIntensity = mapfloat(outputVoltage, 0.99, 2.9, 0.0, 15.0);
 
-  delay(5000);
+  //Serial.print("MP8511 output: ");
+  
+  //Serial.print(uvLevel);
+
+ // Serial.print(" MP8511 voltage: ");
+ // Serial.print(outputVoltage);
+
+  //Serial.print(" UV Intensity (mW/cm^2): ");
+  Serial.println(uvIntensity);  
+  
+  //Serial.println();
+  
+  delay(1000);
+}
+
+//Takes an average of readings on a given pin
+//Returns the average
+int averageAnalogRead(int pinToRead)
+{
+  byte numberOfReadings = 8;
+  unsigned int runningValue = 0; 
+
+  for(int x = 0 ; x < numberOfReadings ; x++)
+    runningValue += analogRead(pinToRead);
+  runningValue /= numberOfReadings;
+
+  return(runningValue);  
+}
+
+//The Arduino Map function but for floats
+//From: http://forum.arduino.cc/index.php?topic=3922.0
+float mapfloat(float x, float in_min, float in_max, float out_min, float out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
